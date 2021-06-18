d057f1970d0862cb4e6dc81b8f1f9cfd94690fad
martinramirezboggio <jramirez.89@yahoo.com>
2021-06-18 12:17:09 +0200

    Add data.

--- constants/dummies.js
+++ constants/dummies.js
@@ -2,14 +2,34 @@ import ExpressionType from "../models/ExpressionType";
 import Expression from "../models/Expression";
 
 export const EXPRESSION_TYPES = [
+  new ExpressionType("actions", "acciones", "accion.png"),
   new ExpressionType("greetings","Saludos", "hello.png"),
   new ExpressionType("food", "Comida", "food.png"),
-  new ExpressionType("wants", "Quiero", "want.png")
+  new ExpressionType("wants", "Quiero", "want.png"),
+  new ExpressionType("family", "Familia", "family.png"),
 ]
 
 export const EXPRESSIONS = [
+  //Acciones
+  new Expression("come", "actions", "Ven", "ven.png", "Ven sound"),
+  new Expression("go", "actions", "Anda", "anda.png", "Anda sound"),
+
   new Expression("hello", "greetings", "Hola", "hello.png", "Hola sound"),
   new Expression("bye", "greetings", "Chau", "bye.png", "Chau sound"),
+
+  //Food
   new Expression("water", "food", "Agua", "water.png", "Agua sound"),
   new Expression("soup", "food", "Sopa", "soup.png", "Sopa sound"),
+
+  //Family
+  new Expression("dad", "family", 'Papá', "papa.png", "Papá sound"),
+  new Expression("mom", "family", 'Mamá', "mama.png", "Mamá sound"),
+  new Expression("sister", "family", 'Hermana', "hermana.png", "Hermana sound"),
+  new Expression("brother", "family", 'Hermano', "hermano.png", "Hermano sound"),
+  new Expression("grandpa", "family", 'Abuelo', "abuelo.png", "Abuelo sound"),
+  new Expression("grandma", "family", 'Abuela', "abuela.png", "Abuela sound"),
+  new Expression("uncle", "family", 'Tío', "tio.png", "Tío sound"),
+  new Expression("aunt", "family", 'Tía', "tia.png", "Tía sound"),
+  new Expression("primo", "family", 'Primo', "primo.png", "Primo sound"),
+  new Expression("prima", "family", 'Prima', "prima.png", "Prima sound"),
 ]
